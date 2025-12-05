65ea41cf1bcc7b790eae0e4951964af74a852037
Aditya <140143477+adityashinde3340@users.noreply.github.com>
2025-12-05 10:46:33 +0530

    addons

--- 3 - Looping Statement/Pattern Assignment/4-12-25/Q20.java
+++ 3 - Looping Statement/Pattern Assignment/4-12-25/Q20.java
@@ -1,1 +1,20 @@
-public class
+public class Q20 {
+    public static void main(String[] args) {
+        int n = 11;   // keep odd for perfect center
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 || i == n-1 || j == 0 || j == n-1 ||
+                    i == j || i + j == n-1 ||
+                    (i == n/2 && j == n/2)) {
+
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
