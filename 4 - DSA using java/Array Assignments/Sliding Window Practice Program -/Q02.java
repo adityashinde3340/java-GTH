7fddca23b1177220f63fd6ef60526f6348535c25
Aditya <140143477+adityashinde3340@users.noreply.github.com>
2026-01-05 12:52:25 +0530

    ..........

--- 4 - DSA using java/Array Assignments/Sliding Window Practice Program -/Q02.java
+++ 4 - DSA using java/Array Assignments/Sliding Window Practice Program -/Q02.java
@@ -6,3 +6,23 @@ Output:
  3 5 7 9
 */
 
+public class Q02{
+	public static void main(String x[]){
+    int k = 2 , sum;
+	int arr[] = {1,2,3,4,5};
+	
+    for(int i = 0 ; i < (arr.length-1) ; i++)
+    {
+		sum = 0;
+		for(int j = i; j<(i+k);j++)
+		{
+			sum = sum + arr[j];
+			
+		}
+		System.out.println(sum+" ");
+	}		
+
+	
+	}
+}
+
---
+++ 4 - DSA using java/Array Assignments/Sliding Window Practice Program -/Q03.java
@@ -0,0 +1,16 @@
+/*
+Q3. Write a java program to Find the window of size K whose elements have the highest sum.
+Input:
+ arr = {4, 2, 10, 3, 8, 6}
+ K = 2
+Output:	13
+Explanation:
+Window {10,3} → sum = 13
+
+*/
+
+public class Q03{
+	public static void main(String x[]){
+		
+	}
+}
