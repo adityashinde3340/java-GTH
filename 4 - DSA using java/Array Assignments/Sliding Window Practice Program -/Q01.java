6b11be12c5e306df4323fef3fa05d9a7887e83d7
Aditya <140143477+adityashinde3340@users.noreply.github.com>
2026-01-05 12:32:25 +0530

    ........

--- 4 - DSA using java/Array Assignments/Sliding Window Practice Program -/Q01.java
+++ 4 - DSA using java/Array Assignments/Sliding Window Practice Program -/Q01.java
@@ -13,8 +13,19 @@ public class Q01{
 	public static void main(String x[]){
 	
 	int arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20};
+	int k = 4 , target = 39 , sum ;
+	int count = 0;
 	
-	
+	for(int i = 0 ; i < (arr.length-k); i++){
+	    sum =0;
+		for(int j = i; j<(k+i);j++){
+			sum = sum + arr[j];
+		}
+		if(sum == target){
+			count++;
+		}
+	}
+	System.out.println("number of windows sum matched = "+count);
 	
 	}
 }
