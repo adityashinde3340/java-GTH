dcbe6499fe299d290cf893be6004be2ca0166cfc
Aditya <140143477+adityashinde3340@users.noreply.github.com>
2026-03-23 16:37:47 +0530

    layout-04

--- OOP/Constructor/Q01.java
+++ OOP/Constructor/Q01.java
@@ -11,3 +11,36 @@ The constructor sets the student’s name and marks.
 Then you apply simple logical condition (≥ 35).
 
 */
+import java.util.Scanner;
+class Student{
+	Scanner sc = new Scanner(System.in);
+	String name = "default";
+	int marks = "0";
+	
+	
+	Student(String name , int marks){
+		System.out.println("enter your name : ");
+		name = sc.nextLine();
+		System.out.println("Enter your marks:");
+		marks = sc.nextInt();
+	}
+	
+	void checkResult(){
+		if(marks >= 35){
+			System.out.println("pass");
+		}
+		else{
+			System.out.println("fail");
+		}
+	}
+	
+}
+
+
+public class Q01{
+	public static void main(String x[]){
+	Student obj = new Student();
+	obj.checkResult();
+	}
+		
+}
