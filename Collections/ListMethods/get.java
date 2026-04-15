cedea5661c3e028970af8c432baaebea12f473b7
Aditya <shindeaditya871@gmail.com>
2026-04-15 16:05:27 +0530

    //...

--- Collections/ListMethods/get.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class get{
-   public static void main(String x[]){
-     List<Integer> ls1 = new Vector<>();
-     ls1.add(10);
-     ls1.add(20);
-     System.out.println(ls1.get(1)+" ");
-   }
-}
